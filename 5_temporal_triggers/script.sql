f0f86fc7071c6b89bd552cabe10f4e9e79c5a674
nathanielmaina <146335108+nathanielmaina@users.noreply.github.com>
2024-11-21 14:02:41 +0300

    Update script.sql
    Signed-off-by: nathanielmaina <146335108+nathanielmaina@users.noreply.github.com>

--- 5_temporal_triggers/script.sql
+++ 5_temporal_triggers/script.sql
@@ -1,1 +1,21 @@
 -- Write your SQL code here
+CREATE TRIGGER after_order_dispatch
+AFTER UPDATE ON `Order`
+FOR EACH ROW
+BEGIN
+    IF NEW.status = 'Shipped' THEN
+        UPDATE Product
+        SET stockQuantity = stockQuantity - NEW.quantity
+        WHERE productId = NEW.productId;
+    END IF;
+END;
+
+CREATE TRIGGER before_delete_old_orders
+BEFORE DELETE ON `Order`
+FOR EACH ROW
+BEGIN
+    IF DATEDIFF(CURRENT_DATE, OLD.orderDate) > 365 THEN
+        INSERT INTO ArchivedOrders SELECT * FROM `Order` WHERE orderId = OLD.orderId;
+    END IF;
+END;
+
