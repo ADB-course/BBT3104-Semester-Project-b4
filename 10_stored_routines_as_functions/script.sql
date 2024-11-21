43e133d628f82312c5cf6fbd63feef44c6bb4817
nathanielmaina <146335108+nathanielmaina@users.noreply.github.com>
2024-11-21 14:10:50 +0300

    Update script.sql
    Signed-off-by: nathanielmaina <146335108+nathanielmaina@users.noreply.github.com>

--- 10_stored_routines_as_functions/script.sql
+++ 10_stored_routines_as_functions/script.sql
@@ -1,1 +1,13 @@
 -- Write your SQL code here
+DELIMITER $$
+
+CREATE FUNCTION GetTotalStockValue()
+RETURNS DECIMAL(10, 2)
+DETERMINISTIC
+BEGIN
+    DECLARE total DECIMAL(10, 2);
+    SELECT SUM(Price * Stock) INTO total FROM Product;
+    RETURN total;
+END $$
+
+DELIMITER ;
